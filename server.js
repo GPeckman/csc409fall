7fec736ac81014b558eec59065ca75918912824a
Garrett Peckman <21979502+GPeckman@users.noreply.github.com>
2025-12-03 17:21:38 -0500

    More node.js stuff

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const path = require('path')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
 // })
+
+app.use(cors())
 
 app.use(express.static(path.join(__dirname, "/")))
 
